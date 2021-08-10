d8731918ffdc77dbc0338e26b74bd9133bd8eea6
Guga <contato.gustavobarbosa@outlook.com>
2021-08-10 15:23:27 -0300

    Preparando revisão do código do Fernando

--- src/services/categoryService.ts
+++
@@ -1,7 +0,0 @@
-import { getRepository } from "typeorm";
-import Category from "../entities/Category";
-
-export async function getCategories() {
-  const categorie = await getRepository(Category).find();
-  return categorie;
-}
